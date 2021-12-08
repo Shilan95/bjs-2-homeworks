cb27c19842e96d0b2ec0fe735b91ca2728cd600a
Шарапов Илья <1l.sharapov@mail.ru>
2021-12-08 13:53:28 +0300

    Выполнено

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,30 @@
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let d = Math.pow(b, 2) - 4 * a * c;
+
+  if (d > 0){
+    return [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)];
+} else if (d === 0) {
+    return [-b / (2 * a)];
+}
+  return []; 
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
-
-  // код для задачи №2 писать здесь
-
+  
+  if (isNaN(percent)) {
+    return `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
+  } else if (isNaN(contribution)) {
+    return `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
+  } else if (isNaN(amount)) {
+    return `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
+  } else {
+    let s = amount - contribution;
+    let n = Math.round((date - new Date()) / 1000 / 60 / 60 / 24 / 30);
+    let p = percent / 100 / 12;
+    let monthlyPayment = s * (p + (p / (((1 + p) ** n) - 1)))
+    totalAmount = (monthlyPayment * n).toFixed(2);
+    console.log(Number(totalAmount));
+  }
   return totalAmount;
 }

7f57bfc316106136bfa1254bead0301e1fa82ee2
Шарапов Илья <1l.sharapov@mail.ru>
2021-12-13 20:19:46 +0300

    Выполнено

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,53 @@
 // Задание 1
 function getArrayParams(arr) {
   let min, max, sum, avg;
+  min = arr[0];
+  max = arr[0];
+  sum = 0;
 
-  // Ваш код
+  for (let index = 0; index < arr.length; index++) {
+    const element = arr[index];
+    if (element > max) {
+      max = element;
+    }
+    if (element < min) {
+      min = element;
+    }
+    sum += element;
+  }
+  avg = (sum / arr.length).toFixed(2);
 
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
 
-  // Ваш код
+  for (let index = 0; index < arr.length; index++) {
+    sum += arr[index];
+  }
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+  let max = 0;
 
-  // Ваш кода
-  // for ...
+  for (let index = 0; index < arrOfArr.length; index++) {
+    const element = arrOfArr[index];
+    const result = func(element);
+    if (result > max) {
+      max = result;
+    }
+  }
   
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  const min = Math.min(...arr);
+  const max = Math.max(...arr);
+  return Math.abs(max - min);
 }

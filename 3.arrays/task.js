ca34a4fd15b117c2237c058bc13ec08c99fc990a
Шарапов Илья <1l.sharapov@mail.ru>
2021-12-18 11:17:05 +0300

    Выполнено

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,15 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
+  if (arr1.length === arr2.length) {
+    result = arr1.every((item, index) => item === arr2[index]);
+  } else {
+    result = false;
+  }
 
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+  return arr.filter((item) => item > 0 && item % 3 === 0).map((item) => item * 10)
 }
